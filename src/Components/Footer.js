4c659118c5895301452523896f89332616436dea
jmajurel <jeanfelix.majurel@gmail.com>
2020-04-01 12:13:01 +0200

    added photo credit to author

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,26 +1,28 @@
 import React from "react";
-import {useTranslation} from "react-i18next"
+import { useTranslation } from "react-i18next";
 import "./Footer.css";
 
-const Footer = () =>  {
-
+const Footer = () => {
   const [t, i18next] = useTranslation();
+
   return (
-  <div className="footer">
-    <p>{t('footer.note')}</p>
-    <p>{t('footer.author')}</p>
-    <div className="contacts">
-      <a
-        className="contact"
-        href="https://www.linkedin.com/in/jean-f%C3%A9lix-majurel-a142235a/"
-      >
-        <i className="fab fa-linkedin" />
-      </a>
-      <a className="contact" href="https://github.com/jmajurel">
-        <i className="fab fa-github" />
-      </a>
+    <div className="footer">
+      <p>{t("footer.note")}</p>
+      <p>{t("footer.author")}</p>
+      <p>{t("footer.bckImgCredit")}</p>
+      <div className="contacts">
+        <a
+          className="contact"
+          href="https://www.linkedin.com/in/jean-f%C3%A9lix-majurel-a142235a/"
+        >
+          <i className="fab fa-linkedin" />
+        </a>
+        <a className="contact" href="https://github.com/jmajurel">
+          <i className="fab fa-github" />
+        </a>
+      </div>
     </div>
-  </div>
-)};
+  );
+};
 
 export default Footer;
