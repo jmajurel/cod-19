ac65140bf791ac0809e26759977035faee32474b
jmajurel <jeanfelix.majurel@gmail.com>
2022-01-02 19:49:18 +0100

    added more tests

--- src/Components/AllGood.test.js
+++ src/Components/AllGood.test.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AllGood from "./AllGood";
 import { MemoryRouter } from "react-router-dom";
+import { act } from "react-dom/test-utils";
+import { render } from "react-dom";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../i18n";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
@@ -11,4 +15,20 @@ it("renders without crashing", () => {
     </MemoryRouter>,
     div
   );
+});
+
+it("renders some info", () => {
+  const container = document.createElement("div");
+  act(() => {
+    render(
+      <I18nextProvider i18n={i18n}>
+        <MemoryRouter>
+          <AllGood />
+        </MemoryRouter>
+      </I18nextProvider>,
+      container
+    );
+  });
+  const generalSentence = container.querySelector("p");
+  expect(generalSentence.textContent.length).toBeGreaterThan(0);
 });
---
+++ src/Components/GraphSelector.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import GraphSelector from "./GraphSelector";
+import renderer from "react-test-renderer";
+import { act } from "react-dom/test-utils";
+import { render } from "react-dom";
+
+it("renders correctly", () => {
+  const tree = renderer.create(<GraphSelector />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("displays label", () => {
+  const mockLabel = "Selection label";
+  const container = document.createElement("div");
+  act(() => {
+    render(<GraphSelector label={mockLabel} />, container);
+  });
+
+  const label = container.querySelector("label");
+  expect(label.textContent).toMatch(mockLabel);
+});
+
+it("reacts to userEvent", () => {
+  const onChange = jest.fn();
+  const container = document.createElement("div");
+  act(() => {
+    render(<GraphSelector handleChange={onChange} />, container);
+  });
+});
